c97b44495894e63118407a5a53223bb607c6d11b
orzorzooo <orzorzooo@gmail.com>
2023-03-08 21:29:08 +0800

    EDIT: compass動畫效果

--- src/views/landing/components/compass.vue
+++ src/views/landing/components/compass.vue
@@ -1,8 +1,6 @@
 <template>
-  {{ bodyW }}
-  <div
-    alt=""
-    class="absolute bg w-1/2 right-0"
+  <!-- {{ bodyW }} -->
+  <!-- <div
     v-motion
     :initial="{
       opacity: 0,
@@ -16,31 +14,79 @@
       y: 300,
       x: 100,
     }"
+  ></div> -->
+  <div
+    alt=""
+    class="absolute right-0"
+    id="compass"
+    v-motion
+    :initial="{
+      opacity: 0,
+      y: 600,
+    }"
+    :enter="{
+      opacity: 0.7,
+      transition: {
+        delay: 750,
+      },
+      y: 300,
+      x: 100,
+    }"
   >
-    <img :src="compass_body" class="absolute inset-0" ref="body" id="body" />
-    <img :src="compass_hand" ref="hand" id="hand" />
+    <img :src="compass_body" class="absolute inset-0" ref="cbody" id="cbody" />
+    <img
+      v-motion
+      :initial="{
+        rotate: -150,
+      }"
+      :enter="{
+        rotate: 30,
+        transition: {
+          repeat: Infinity,
+          repeatType: 'mirror',
+          duration: 3000,
+        },
+      }"
+      :src="compass_hand"
+      class="relative"
+      ref="chand"
+      id="chand"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, nextTick } from "vue";
+import { ref, onMounted, nextTick, onUpdated, watch } from "vue";
 import { useElementSize, useDeviceMotion } from "@vueuse/core";
 import compass_body from "/src/assets/images/compass.svg";
 import compass_hand from "/src/assets/images/compass_hand.svg";
-const body = ref(null);
-const hand = ref(null);
-const { width: bodyW, height: bodyH } = useElementSize(body);
-const { acceleration, accelerationIncludingGravity, rotationRate, interval } =
-  useDeviceMotion();
-console.log("fuc22", acceleration);
-function setHand() {
-  const hand = document.querySelector("#hand");
-  const body = document.querySelector("#body");
-  console.log(body.offsetHeight);
-  hand.style.height = `300px`;
-}
+const cbody = ref(null);
+// const hand = ref(null);
+// const { width: bodyW, height: bodyH } = useElementSize(cbody);
+const { width: bodyW, height: bodyH } = useElementSize(cbody);
+const { acceleration, accelerationIncludingGravity, rotationRate, interval } = useDeviceMotion();
+
+watch(bodyW, (v, oldv) => {
+  const cbody = document.querySelector("#cbody");
+  const chand = document.querySelector("#chand");
+
+  chand.style.height = `${v}px`;
+  const chand_offset = chand.offsetWidth / 2;
+  console.log(chand_offset);
+  chand.style.left = `${v * 0.5 - chand_offset}px`;
+});
+
 onMounted(() => {
-  nextTick();
-  setHand();
+  // console.log("1", cbody.value);
+  // console.log("2", bodyW.value);
 });
 </script>
+<style scoped>
+#compass {
+  width: 50%;
+}
+
+/* #hand {
+  position: relative;
+} */
+</style>
